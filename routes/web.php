3dd072d5e5476779f6dfe1a308d80585502f8aac
hosni <mohamedhosni@192.168.1.7>
2022-01-24 19:39:57 +0200

    checkout page

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
+
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\PackageController;
+use Illuminate\Http\Request;
+
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,6 +18,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/packages/{package}', [PackageController::class, 'show']);
-Route::get('/', [HomeController::class,'homepage']);
+Route::get('/checkout', function (Request $request) {
 
-
+    return view('checkout', compact('request'));
+})->name('checkout');
+Route::get('/', [HomeController::class, 'homepage']);
